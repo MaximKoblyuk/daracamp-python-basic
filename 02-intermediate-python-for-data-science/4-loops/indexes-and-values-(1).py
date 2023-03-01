19efd4b855ab376a7c400bdb2a4327db664396ed
Maxim Koblyuk <89156031+MaximKoblyuk@users.noreply.github.com>
2023-03-01 13:44:11 +0100

    Update indexes-and-values-(1).py

--- 02-intermediate-python-for-data-science/4-loops/indexes-and-values-(1).py
+++ 02-intermediate-python-for-data-science/4-loops/indexes-and-values-(1).py
@@ -17,7 +17,6 @@ Instructions
 # areas list
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 
-# Change for loop to use enumerate()
-for index, a in enumerate(areas):
-    print("room " + str(index) + ": " + str(a))
-    
+# Adapt the printout
+for index, area in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
