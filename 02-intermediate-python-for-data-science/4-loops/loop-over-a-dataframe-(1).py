8e711d27364af4f95cd5a33afb2d1865cc4f2f6e
Maxim Koblyuk <89156031+MaximKoblyuk@users.noreply.github.com>
2023-03-01 13:49:40 +0100

    Update loop-over-a-dataframe-(1).py

--- 02-intermediate-python-for-data-science/4-loops/loop-over-a-dataframe-(1).py
+++ 02-intermediate-python-for-data-science/4-loops/loop-over-a-dataframe-(1).py
@@ -20,7 +20,6 @@ rows contents.
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
 
-# Iterate over rows of cars
-for lab, row in cars.iterrows():
-    print(lab)
-    print(row)
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(lab + ": " + str(row['cars_per_cap']))
