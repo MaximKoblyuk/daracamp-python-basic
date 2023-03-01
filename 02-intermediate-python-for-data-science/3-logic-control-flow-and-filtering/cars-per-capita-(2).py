e90e3ba81ca809c9273e6422bd5138547633bd2a
Maxim Koblyuk <89156031+MaximKoblyuk@users.noreply.github.com>
2023-03-01 13:38:17 +0100

    Update cars-per-capita-(2).py

--- 02-intermediate-python-for-data-science/3-logic-control-flow-and-filtering/cars-per-capita-(2).py
+++ 02-intermediate-python-for-data-science/3-logic-control-flow-and-filtering/cars-per-capita-(2).py
@@ -13,25 +13,7 @@ Instructions
 -Use the code sample above to create a DataFrame medium, that includes all the observations of cars that have a cars_per_cap between 100 and 500.
 -Print out medium.
 '''
-# Import cars data
-import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
 
-# Import numpy, you'll need this
-import numpy as np
-
-# Create medium: observations with cars_per_cap between 100 and 500
-cpc = cars['cars_per_cap']
-between = np.logical_and(cpc > 10, cpc < 80)
-medium = cars[between]
-
-# Print medium
-print(medium)
-# Import cars data
-import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
-
-# Import numpy, you'll need this
 import numpy as np
 
 # Create medium: observations with cars_per_cap between 100 and 500
